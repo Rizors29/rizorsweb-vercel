548d4cc3df8b1af4ff0dc8d84df80d1e68059220
Rizors29 <akunduawerewolf@gmail.com>
2026-02-07 18:20:25 +0700

    first commit

---
+++ api/views.js
@@ -0,0 +1,10 @@
+import { kv } from '@vercel/kv';
+
+export default async function handler(req, res) {
+  try {
+    const views = await kv.incr('rizorsweb:views');
+    res.status(200).json({ views });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to update views' });
+  }
+}
