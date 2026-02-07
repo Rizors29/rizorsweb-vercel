804df6f724ec5536891b2d41238bd939cbaf8964
Rizors29 <akunduawerewolf@gmail.com>
2026-02-07 20:07:22 +0700

    fix api views

--- api/views.js
+++ api/views.js
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
     const views = await kv.incr('rizorsweb:views');
     res.status(200).json({ views });
   } catch (error) {
+    console.error('KV ERROR:', error);
     res.status(500).json({ error: 'Failed to update views' });
   }
 }
