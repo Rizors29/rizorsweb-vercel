4943f1d7f8255222b19d2cc44a0c1bdb843215b6
Rizors29 <akunduawerewolf@gmail.com>
2026-02-08 19:58:38 +0700

    update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -11,12 +11,13 @@ function Layout() {
           fixed inset-0 -z-10
           bg-[url(/img/background.png)]
           bg-cover
-          bg-[position:15%_center]
-          md:bg-center
+          bg-center
+          will-change-transform
+          transform translate-z-0
         "
       />
 
-      <main className="w-full min-h-[100svh] overflow-hidden">
+      <main className="w-full min-h-[100svh] overflow-x-hidden">
         <Outlet />
       </main>
     </>
