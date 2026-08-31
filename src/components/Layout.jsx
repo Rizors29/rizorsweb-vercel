9188d321532133fbb011a1a148c9e318764b8b53
Rizors29 <akunduawerewolf@gmail.com>
2026-08-31 09:40:20 +0700

    penambahan copyright

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -11,6 +11,10 @@ function Layout() {
       <main className="w-full min-h-[100svh] overflow-hidden">
         <Outlet />
       </main>
+
+      <footer className="fixed bottom-0 w-full py-3 text-center text-sm text-white/50 z-10">
+        &copy; {new Date().getFullYear()} RizorsWeb
+      </footer>
     </>
   );
 }
